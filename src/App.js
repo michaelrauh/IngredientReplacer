0d45a1d3cf4416a0cee111579d1fb508a8f79789
michaelrauh <michaelrauh@ymail.com>
2017-01-16 12:44:16 -0500

    initial bootstrapped react project, with example from react docs dropped in to make textfield show below

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {value: 'Type some *markdown* here!'};
+  }
+
+  handleChange() {
+    this.setState({value: this.refs.textarea.value});
+  }
+
+  getRawMarkup() {
+    return { __html:this.state.value };
+  }
+
+  render() {
+    return (
+      <div className="MarkdownEditor">
+        <h3>Input</h3>
+        <textarea
+          onChange={this.handleChange}
+          ref="textarea"
+          defaultValue={this.state.value} />
+        <h3>Output</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={this.getRawMarkup()}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
